4391ef9ae674b8bf1c0dc0c9377d2ba24d03501b
Evsyukov Denis <denis@evsyukov.org>
2022-09-11 13:51:57 +0300

    fix: small typo fix

--- scripts/getpocket.sh
+++ scripts/getpocket.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 FILE=./linkding_getpocket.txt
 
 curl --silent https://getpocket.com/users/juev/feed/all | xmllint --xpath '//rss/channel/item/link/text()' - > $FILE
-# Reading the URLs one by one and adding to shiori
+# Reading the URLs one by one and adding to linkding
 while IFS= read -r line; do
 	curl -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Authorization: Token ${LINKDING_TOKEN}" https://link.juev.org/api/bookmarks/ -d "{\"url\":\"${line}\"}"
 done < $FILE
