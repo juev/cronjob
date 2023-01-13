3b73adefd2044a214018fecd5fbe4506e6ed05ff
Evsyukov Denis <denis@evsyukov.org>
2023-01-13 13:54:26 +0300

    feat: use rss-parse for process feeds

--- scripts/getpocket.sh
+++ scripts/getpocket.sh
@@ -3,12 +3,14 @@
 set -euo pipefail
 
 FILE=./linkding_getpocket.txt
+wget https://github.com/juev/rss-parser/releases/latest/download/rss-parser-linux-amd64 -O run
+chmod +x run
 
-curl --silent https://getpocket.com/users/juev/feed/all | xmllint --xpath '//rss/channel/item/link/text()' - > $FILE
+./run https://getpocket.com/users/juev/feed/all > $FILE
 # Reading the URLs one by one and adding to linkding
 while IFS= read -r line; do
 	curl -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Authorization: Token ${LINKDING_TOKEN}" https://link.juev.org/api/bookmarks/ -d "{\"url\":\"${line}\"}"
 done < $FILE
 
-rm $FILE
+rm run $FILE
 exit 0
