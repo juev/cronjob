fe87c8838d37ec560def22da435f0abed1306b47
Evsyukov Denis <114635+juev@users.noreply.github.com>
2023-01-19 18:31:34 +0300

    Update getpocket.sh

--- scripts/getpocket.sh
+++ scripts/getpocket.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 FILE=./linkding_getpocket.txt
 curl --retry-all-errors --retry 10 --retry-delay 0 --retry-max-time 40 --max-time 10 \
  https://github.com/juev/rss-parser/releases/latest/download/rss-parser-linux-amd64 \
- -L -o run
+ -L -s -o run
 chmod +x run
 
 ./run https://getpocket.com/users/juev/feed/all > $FILE
