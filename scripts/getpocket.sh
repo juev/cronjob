23ec9612c4070a63c7734cf8449bfd2d122e2b34
Evsyukov Denis <denis@evsyukov.org>
2023-01-19 18:18:47 +0300

    feat: wget -> curl

--- scripts/getpocket.sh
+++ scripts/getpocket.sh
@@ -3,7 +3,9 @@
 set -euo pipefail
 
 FILE=./linkding_getpocket.txt
-wget --tries=10 --random-wait https://github.com/juev/rss-parser/releases/latest/download/rss-parser-linux-amd64 -O run
+curl --retry-all-errors --retry 10 --retry-delay 0 --retry-max-time 40 --max-time 10 \
+ https://github.com/juev/rss-parser/releases/latest/download/rss-parser-linux-amd64 \
+ -L -o run
 chmod +x run
 
 ./run https://getpocket.com/users/juev/feed/all > $FILE
