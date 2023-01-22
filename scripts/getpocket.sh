00d2a9f52606a6b2f6984fb84dbf36064dcaaa36
Evsyukov Denis <denis@evsyukov.org>
2023-01-22 10:21:14 +0300

    feat: upgrade rss-parser

--- scripts/getpocket.sh
+++ scripts/getpocket.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 FILE=./linkding_getpocket.txt
 
-/app/rss-parser https://getpocket.com/users/juev/feed/all > $FILE
+rss-parser https://getpocket.com/users/juev/feed/all > $FILE
 # Reading the URLs one by one and adding to linkding
 while IFS= read -r line; do
 	curl -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Authorization: Token ${LINKDING_TOKEN}" https://link.juev.org/api/bookmarks/ -d "{\"url\":\"${line}\"}"
