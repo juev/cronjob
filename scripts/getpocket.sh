f0e569a74e41e924a852f509249c377c171504aa
Evsyukov Denis <114635+juev@users.noreply.github.com>
2024-02-23 23:13:02 +0300

    Update getpocket.sh

--- scripts/getpocket.sh
+++ scripts/getpocket.sh
@@ -7,7 +7,7 @@ FILE=./linkding_getpocket.txt
 rss-parser https://getpocket.com/users/juev/feed/all > $FILE
 # Reading the URLs one by one and adding to linkding
 while IFS= read -r line; do
-	curl -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Authorization: Token ${LINKDING_TOKEN}" https://link.juev.org/api/bookmarks/ -d "{\"url\":\"${line}\"}"
+	curl -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Authorization: Token ${LINKDING_TOKEN}" https://links.evsyukov.org/api/bookmarks/ -d "{\"url\":\"${line}\"}"
 done < $FILE
 
 rm $FILE
